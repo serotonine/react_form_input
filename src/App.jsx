40b729104d86502a239e314da63b64edc16d311f
serotonine <ks.serotonine@gmail.com>
2024-03-12 12:03:05 +0100

    Validate form on onSubmit event.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Header from "./components/Header.jsx";
 import Login from "./components/Login.jsx";
-import LoginUseState from "./components/LoginUseState.jsx";
 function App() {
   return (
     <>
       <Header />
       <main>
-        <LoginUseState />
+        <Login />
       </main>
     </>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,26 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 export default function Login() {
+  // Require less code than with useState().
   const email = useRef();
   const password = useRef();
+  // Validation
+  const [isInvalidEmail, setIsInvalidEmail] = useState(false);
   // Submit
   function handleSubmit(evt) {
     evt.preventDefault();
     const entEmail = email.current.value;
     const entPassword = password.current.value;
+    if (!entEmail.includes("@")) {
+      setIsInvalidEmail(true);
+      // Stop submit logic.
+      return;
+    }
+
     console.log(entEmail, entPassword);
+  }
+  function handleEmailFocus() {
+    // Reset
+    setIsInvalidEmail(false);
   }
 
   return (
@@ -17,7 +30,18 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={email} />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            ref={email}
+            onFocus={handleEmailFocus}
+          />
+          {isInvalidEmail && (
+            <div className="control-error">
+              Please enter a valid email address
+            </div>
+          )}
         </div>
 
         <div className="control no-margin">
