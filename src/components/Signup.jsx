246554b1dae587910b388cfe9e30114a8e5c208d
serotonine <ks.serotonine@gmail.com>
2024-03-12 12:15:53 +0100

    Validate form in-built html validation.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -16,6 +16,7 @@ export default function Signup() {
     _values.acquisition = _multipleValues;
     // Native method reset.
     evt.target.reset();
+    // VALIDATION BUILT IN //
 
     console.log(_values);
   }
@@ -26,13 +27,19 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            minLength={8}
+          />
         </div>
 
         <div className="control">
@@ -41,6 +48,8 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            required
+            minLength={8}
           />
         </div>
       </div>
@@ -50,12 +59,12 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" required />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" required />
         </div>
       </div>
 
